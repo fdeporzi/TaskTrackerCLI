a19e824ed42c955fe988b865c02d4b8e3cf79cb4
fdeporzi <fdeporzi@asu.edu>
2025-03-16 21:38:51 -0700

    First Commit

---
+++ task-cli.py
@@ -0,0 +1,16 @@
+# add, update delete tasks
+# list tasks
+# mark tasks as done or in progress
+# list tasks that are done
+# list tasks that are in progress
+# list tasks that are not done
+
+"""
+You can use any programming language to build this project.
+Use positional arguments in command line to accept user inputs.
+Use a JSON file to store the tasks in the current directory.
+The JSON file should be created if it does not exist.
+Use the native file system module of your programming language to interact with the JSON file.
+Do not use any external libraries or frameworks to build this project.
+Ensure to handle errors and edge cases gracefully.
+"""
