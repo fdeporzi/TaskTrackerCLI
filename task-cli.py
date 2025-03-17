4108ae52ec8d998290e63ed0a704cfbb3f559058
fdeporzi <fdeporzi@asu.edu>
2025-03-16 22:10:07 -0700

    Created main function and program loop

--- task-cli.py
+++ task-cli.py
@@ -1,10 +1,30 @@
-print("Welcome to Task CLI")
-print("Type 'help' to see the list of commands")
+def main():
+    print("Welcome to Task CLI")
+    print("Type 'help' to see the list of commands")
+
+    while True:
+        user_input = input("Enter a command: ")
+
+        #exit command
+        if user_input.lower() == "exit":
+            print("Exiting Task CLI...")
+            break
+
+if __name__ == "__main__":
+    main()
 
 
 
 
 
+
+
+
+# 1. create loop to keep the program running
+# 2. get user input
+# 3. check if user input is valid
+# 4. if valid, run the command
+# 5. if invalid, print error message
 
 
 
